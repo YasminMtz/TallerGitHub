de91b57e2ef2fca01f80f62dbedc054c7c2255bb
YasminMtz <martinezescobaryasmin7@gmail.com>
2023-11-22 17:50:35 -0600

    Agrega titulo en X

--- taller_grafica.py
+++ taller_grafica.py
@@ -7,12 +7,12 @@ plt.style.use('_mpl-gallery-nogrid')
 np.random.seed(1)
 x = np.random.randn(5000)
 y = 1.2 * x + np.random.randn(5000) / 3
-
+#cambiossss
 # plot:
 fig, ax = plt.subplots()
 
 ax.hexbin(x, y, gridsize=20)
-
+plt.xlabel('Eje de las x')
 ax.set(xlim=(-2, 2), ylim=(-3, 3))
 
 plt.show()
